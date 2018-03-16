ea29f165c23f754c7e4e450acc46394744f5c377
Muthukumarll <35914910+Muthukumarll@users.noreply.github.com>
2018-03-16 15:43:21 +0530

    Create p19.c

---
+++ p19.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	int a,b,c;
+	int i;
+clrscr();
+		scanf("%d %d",&a,&b);
+		if(i%2==0)
+		c=a/b;
+		else
+		c=a%b;
+		printf("\n%d",c);
+	getch();
+	return 0;
+}
